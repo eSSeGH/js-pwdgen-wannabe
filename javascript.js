fd297b82b5460ddaa63f0637e5795398f178bddb
eSSeGH <ste.virgadaula@gmail.com>
2023-01-30 16:04:39 +0100

    EDIT details + JS password gen & message added

--- javascript.js
+++ javascript.js
@@ -23,4 +23,26 @@ console.log("Dichiaro var. favoriteNumber")
 let favoriteNumber = prompt("Quale'è il tuo numero preferito?")
 console.log("la variabile favoriteNumber é: ", favoriteNumber)
 
-// Infine scrivi sulla pagina il risultato della concatenazione 
+// Infine scrivi sulla pagina il risultato della concatenazione ****************
+
+// dichiara una variabile password 
+
+console.log("Dichiaro una variabile password")
+let password = (firstName + lastName + favoriteColor + favoriteNumber)
+console.log("la variabile password é: ", password)
+
+// richiama il div generated-password per inserire la variabile password
+
+console.log("dichiaro una costante associata a generated-password")
+const generatedPassword = document.getElementById("generated-password")
+console.log("la costante generatedPassword è: generatedPassword")
+
+// Modifico il contenuto di generated password con la variabile password
+
+console.log("Modifico il contenuto di generated password con la variabile password")
+generatedPassword.innerHTML = password
+
+// messaggio Finale ***********************************************
+
+console.log("pop-up messaggio finale")
+alert("La tua Password è stata generata con successo!")
