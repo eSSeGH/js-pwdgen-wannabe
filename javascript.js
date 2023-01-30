134d99b220f7ac5df8594792e82ea0b4a59843f9
eSSeGH <ste.virgadaula@gmail.com>
2023-01-30 15:35:12 +0100

    FEAT  main content + JS all prompts added

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+// Chiedi all’utente il suo nome
+
+console.log("Dichiaro var. firstName")
+let firstName = prompt("Quale'è il tuo nome?")
+console.log("la variabile firstName é: ", firstName)
+
+// poi chiedi il suo cognome
+
+console.log("Dichiaro var. lastName")
+let lastName = prompt("Quale'è il tuo cognome?")
+console.log("la variabile lastName é: ", lastName)
+
+// poi chiedi il suo colore preferito
+
+console.log("Dichiaro var. favoriteColor")
+let favoriteColor = prompt("Quale'è il tuo colore preferito?")
+console.log("la variabile favoriteColor é: ", favoriteColor)
+
+// e un numero
+
+console.log("Dichiaro var. favoriteNumber")
+let favoriteNumber = prompt("Quale'è il tuo numero preferito?")
+console.log("la variabile favoriteNumber é: ", favoriteNumber)
+
+// Infine scrivi sulla pagina il risultato della concatenazione 
